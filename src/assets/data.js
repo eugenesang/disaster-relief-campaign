8be7dd11382eb5c32bc5247dd4d20d5a9b0e6a88
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-01 20:14:30 +0300

    raw data

---
+++ src/assets/data.js
@@ -0,0 +1,93 @@
+import usaImg1 from "./images/carlifonia_floods_1.webp";
+import usaImg2 from "./images/carlifonia_floods_2.jpg";
+import usaImg3 from "./images/usa_floods_1.webp";
+import indonesiaImg1 from "./images/indonesia_floods_1.webp"
+import indonesiaImg2 from "./images/indonesia_floods_2.jpg"
+import indonesiaImg3 from "./images/indonesia_floods_3.jpg"
+import dubaiImg1 from "./images/dubai_floods_1.jpg"
+import dubaiImg2 from "./images/dubai_floods_2.webp"
+import dubaiImg3 from "./images/dubai_floods_3.webp"
+import kenyaImg1 from "./images/kenya_floods_1.jpg"
+import kenyaImg2 from "./images/kenya_floods_2.jpg"
+import kenyaImg3 from "./images/kenya_floods_3.jpeg"
+import ukImg1 from "./images/uk_floods_1.jpg"
+import ukImg2 from "./images/uk_floods_2.jpeg"
+import ukImg3 from "./images/uk_floods_3.jpg"
+
+export const places = [
+    {
+        country: "USA",
+        excerpt: "Floods caused destructions of property including power lines",
+        img: usaImg1
+    },
+    {
+        country: "USA",
+        excerpt: "California rogue floods caused allot of property damage",
+        img: usaImg2
+    },
+    {
+        country: "USA",
+        excerpt: "Local displacement in San Jose, California due to floods",
+        img: usaImg3
+    },
+    {
+        country: "Indonesia",
+        excerpt: "Floods submerged streets and destroyed infrastructure",
+        img: indonesiaImg1
+    },
+    {
+        country: "Indonesia",
+        excerpt: "Houses and buildings inundated by torrential floodwaters",
+        img: indonesiaImg2
+    },
+    {
+        country: "Indonesia",
+        excerpt: "Communities evacuated as floodwaters rise",
+        img: indonesiaImg3
+    },
+    {
+        country: "UAE",
+        excerpt: "Floodwaters engulfed urban areas, causing widespread damage",
+        img: dubaiImg1
+    },
+    {
+        country: "UAE",
+        excerpt: "Vehicles stranded in flooded streets after heavy rains",
+        img: dubaiImg2
+    },
+    {
+        country: "UAE",
+        excerpt: "Floods disrupted transportation networks and infrastructure",
+        img: dubaiImg3
+    },
+    {
+        country: "Kenya",
+        excerpt: "Floods submerged a neighbourhood, impacting health & hygiene",
+        img: kenyaImg1
+    },
+    {
+        country: "Kenya",
+        excerpt: "Aftermath of a destructive rain in a slum in Nairobi",
+        img: kenyaImg2
+    },
+    {
+        country: "Kenya",
+        excerpt: "Red cross response team on the ground offering help",
+        img: kenyaImg3
+    },
+    {
+        country: "UK",
+        excerpt: "Floodwaters inundated homes and businesses along rivers",
+        img: ukImg1
+    },
+    {
+        country: "UK",
+        excerpt: "Emergency response teams rescue stranded residents",
+        img: ukImg2
+    },
+    {
+        country: "UK",
+        excerpt: "Floods wreak havoc on coastal communities, displacing many",
+        img: ukImg3
+    }
+]
