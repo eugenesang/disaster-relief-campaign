b158c4fa74119db30a7b805ebbf31a1bc4db2613
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-05-01 20:11:23 +0300

    app entry file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Gallery from "./components/Gallery";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Header/>
+     <Gallery/>
+    </div>
+  );
+}
+
+export default App;
